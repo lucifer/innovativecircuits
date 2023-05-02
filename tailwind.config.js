1e3177df3587d296854b72255144e61ff9205793
Lucifer <lucifer.stevenson@gmail.com>
2023-05-02 16:56:23 -0400

    Try netlifyCLI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./dist/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
       fontFamily: {
