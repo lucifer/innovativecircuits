d09f1d4424c3c57626c98b76c3e86ec9a2558f6a
Lucifer <lucifer.stevenson@gmail.com>
2023-05-02 18:07:18 -0400

    Move all to dist /

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}", ".//dist/index.html"],
+  content: ["./dist/**/*.{html,js}", "./dist/index.html"],
   theme: {
     extend: {
       fontFamily: {
